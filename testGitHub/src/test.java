4f7b7901ca5e56febf23f126b514a8be8ef787d9
adamly <adamlyz@hotmail.com>
2017-03-10 16:33:58 +0800

    adadadfsa

---
+++ testGitHub/src/test.java
@@ -0,0 +1,15 @@
+import java.nio.charset.Charset;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.*;
+
+public class test {
+    public static void main(String[] args) {
+    	
+    	System.out.println("Hello World");;
+    
+    }
+}
